95ebd13becdcbd2315cd4b07f169a154f56dd810
Mydayyy <mydayyy@mydayyy.eu>
2021-02-24 01:24:27 +0100

    Optimized process of sending initial map data fixes #2

--- ServerSideMap/_Minimap.cs
+++ ServerSideMap/_Minimap.cs
@@ -7,6 +7,17 @@ namespace ServerSideMap
     // ReSharper disable once InconsistentNaming
     public class _Minimap
     {
+        public static Minimap _instance;
+        
+        [HarmonyPatch(typeof(Minimap), "Awake")]
+        public static class Awake
+        {
+            private static void Postfix(Minimap __instance)
+            {
+                _instance = __instance;
+            }
+        }
+        
             [HarmonyReversePatch]
             [HarmonyPatch(typeof(Minimap), "Explore", typeof(int), typeof(int))]
             public static bool Explore(Minimap instance, int x, int y)
