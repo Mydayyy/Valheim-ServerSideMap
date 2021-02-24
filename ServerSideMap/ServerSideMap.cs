a79b9791ba2f5155ab4731edc1c8ef5fdf1c1467
Mydayyy <mydayyy@mydayyy.eu>
2021-02-24 01:37:42 +0100

    Updated Version

--- ServerSideMap/ServerSideMap.cs
+++ ServerSideMap/ServerSideMap.cs
@@ -7,7 +7,7 @@ using HarmonyLib;
 
 namespace ServerSideMap
 {
-    [BepInPlugin("eu.mydayyy.plugins.serversidemap", "ServerSideMap", "1.0.0.0")]
+    [BepInPlugin("eu.mydayyy.plugins.serversidemap", "ServerSideMap", "1.1.0.0")]
     public class ServerSideMap : BaseUnityPlugin
     {
         void Awake()
